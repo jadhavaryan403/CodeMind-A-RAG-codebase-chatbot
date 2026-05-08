1383ffe53fca7bba60f54069f141b7119d02ce58
Aryan Jadhav <jadhavaryan403@gmail.com>
2026-05-08 20:29:52 +0530

    Add PostgreSQL database support for deployment

--- codebase_assistant/settings.py
+++ codebase_assistant/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 from pathlib import Path
 from dotenv import load_dotenv
 import os
+import dj_database_url
 
 load_dotenv()
 
@@ -119,10 +120,11 @@ LOGGING = {
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(
+        os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
